bfab349f934456b2cff38762fbaaf3d20f63f43b
Gabriel Innocent <okellogabrielinnocent@gmail.com>
2018-07-24 13:30:06 +0300

    Initial commit

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
+from api.models import DatabaseConnection
 from api.views import app
 
 if __name__ == '__main__':
+    database_connection = DatabaseConnection()
+
+    database_connection.create_tables()
     app.run(debug=True)
+
